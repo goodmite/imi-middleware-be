d2dc8b684224bc822bf5f5b8d95892522743b1e0
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2020-04-23 18:52:39 +0530

    deploy

--- src/socket-event-map.ts
+++ src/socket-event-map.ts
@@ -1,7 +1,7 @@
 export const allowedNameSpace = ['BOT', 'CONNECT'];
 export const namespaceEventMap = {
   BOT: {
-    events: ['preview', 'chat', 'train', 'previewStyle', 'consumer_search', 'deploy_jenkins', 'test'],
+    events: ['preview', 'chat', 'train', 'previewStyle', 'consumer_search', 'deploy_jenkins', 'test', 'bot_testing'],
   },
 };
 
